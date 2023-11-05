4dc78a9a6017605e0070973bcac0ba084854e09f
Nathan Farlow <natethegreatest11@gmail.com>
2023-11-04 20:03:05 -0400

    Fix stuff

--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,8 @@ from main import generate_meme
 import discord
 from discord.ext import commands
 
-bot = commands.Bot()
+intents = discord.Intents.all()
+bot = commands.Bot(intents=intents)
 
 
 @bot.slash_command(name="meme", guild_ids=[1170471455146909757])
@@ -15,17 +16,17 @@ async def meme(ctx, description: str):
         await ctx.respond("couldn't make it work :( try again")
 
 
-# listen for a user to send a message, and if they did, respond to it
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
+
+    content = str(message.content)
     if message.author.id == 193479248294445056:
-        meme = generate_meme(
-            'nerd emoji with glasses gif with caption """message.content"""'
-        )
+        meme = generate_meme(f"nerd emoji glasses gif with caption '''\"{content}\"'''")
         if meme:
             await message.channel.send(file=discord.File(meme.name))
+    print(content)
 
 
 bot.run(open(".env", "r").read())
